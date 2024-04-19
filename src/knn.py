ba108371ddd1a3ca11cda0fae9b09874cf0508d5
Bruno Pedrazza <bruno.pedrazza@transfero.com>
2024-04-19 02:32:18 -0300

    Replace eucledian distance method

--- src/knn.py
+++ src/knn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 
+from scipy.spatial import distance
 from utils import divide_chunks, measure_execution_time
 
 
@@ -24,33 +25,37 @@ class KNN:
         self._y_train_indices = indices.reshape((-1, 1))
 
     
-    @staticmethod
-    def __euclidean_distances(X_train, X_test):
-        return np.sqrt(np.sum(np.square(np.expand_dims(X_test, axis=1) - X_train), axis=2))
+    # @staticmethod
+    # def __euclidean_distances(X_train, X_test):
+    #     return distance.cdist(X_test, X_train, 'euclidean')
+        # return np.sqrt(np.sum(np.square(np.expand_dims(X_test, axis=1) - X_train), axis=2))
     
     def __compute_distances(self, X_test, n_chunks):
-        distances = np.empty((X_test.shape[0], self.k), dtype=np.int64)
+        distances = np.empty((X_test.shape[0], self.k), dtype=np.int32)
 
         for i, X_test_chunk in enumerate(divide_chunks(X_test, n_chunks)):
             start = i * n_chunks
             end = start + X_test_chunk.shape[0]
-            dists = self.__euclidean_distances(self._X_train, X_test_chunk)
+            dists = distance.cdist(X_test_chunk, self._X_train, 'euclidean')
             distances[start:end, :] = np.argsort(dists, axis=1)[:, :self.k]
 
         return distances
 
     @measure_execution_time
     def predict(self, X_test, n_chunks=256):
-        X_test = np.array(X_test)
-        distances = self.__compute_distances(X_test, n_chunks)
+        _X_test = np.array(X_test)
+        _classes = self._classes
+        _y_train_idxs = self._y_train_indices
 
-        n_classes = len(self._classes)
-        n_X_test = len(X_test)
+        distances = self.__compute_distances(_X_test, n_chunks)
 
-        y_pred = np.empty((n_X_test, n_classes), dtype=self._classes[0].dtype)
-        mode = scipy.stats.mode(self._y_train_indices[distances], axis=1, keepdims=True).mode
+        n_classes = len(_classes)
+        n_X_test = len(_X_test)
+
+        y_pred = np.empty((n_X_test, n_classes), dtype=_classes[0].dtype)
+        mode = scipy.stats.mode(_y_train_idxs[distances], axis=1, keepdims=True).mode
         mode = np.asarray(np.ravel(mode))
-        y_pred = self._classes.take(mode)
+        y_pred = _classes.take(mode)
 
         return np.ravel(y_pred)
     
