0b6fce5312ea20673d85a8af89204cf2c218ab8d
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-04-29 09:40:41 -0300

    Start testing classification using kd tree with toy datasets

--- src/acc_test_classification.py
+++ src/acc_test_classification.py
@@ -3,18 +3,26 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as metrics
 
+from kd_tree import KdTree
 from knn import KNN
 
 if __name__ == "__main__":
     dataset = datasets.load_digits()
 
+
+    # dataset = datasets.fetch_covtype()
+    # data, target = datasets.make_classification(n_samples=10000, n_features=50, n_classes=10, n_informative=10, shuffle=True, random_state=42)
+
     X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=0.1, random_state=42)
 
+    kd_tree = KdTree.construct(X_train, y_train)
+    y_pred = kd_tree.predict(X_test)
+    # l = kd_tree.dfs()
+    
+    # for e in l:
+    #     print(e[1])
     # my implemenation
-    knn = KNN(k=3)
-    knn.fit(X_train, y_train)
-    y_pred = knn.predict(X_test)
-
-    acc = metrics.accuracy_score(y_test, y_pred)
-    print(f"acc: {acc:.6f}")
+    # knn = KNN(k=9)
+    # knn.fit(X_train, y_train)
+    # y_pred = knn.predict(X_test, n_chunks=256)
 
