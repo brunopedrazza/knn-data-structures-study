f5720a2f44e70175c51ca8281121b38150bc02f2
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-04-29 09:38:17 -0300

    Implement method to calculate eucledian distance on utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from time import perf_counter
 
+from scipy.spatial import distance
 
 def divide_chunks(l, n):
     for i in range(0, len(l), n):  
@@ -14,3 +15,9 @@ def measure_execution_time(func):
         print(f"Function {func.__qualname__} took {toc - tic:.4f} seconds to execute")
         return result
     return wrapper
+
+def euclidean_distance(p1, p2):
+    if len(p1.shape) == 1:
+        return distance.euclidean(p1, p2)
+    return distance.cdist(p1, p2, 'euclidean')
+
