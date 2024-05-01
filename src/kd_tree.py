14b20a1a2aeb33a0725b051bbbeaeae3986641a8
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-05-01 20:33:55 -0300

    Change kd trees tests to use Node

--- src/kd_tree.py
+++ src/kd_tree.py
@@ -1,14 +1,7 @@
 import numpy as np
+
+from node import Node
 from utils import euclidean_distance, measure_execution_time
-
-class Node:
-    def __init__(self, point, class_):
-        self.point = point
-        self.class_ = class_
-        self.distance = None
-
-    def __str__(self):
-        return '%s' % self.point + (str(self.class_) if self.class_ else "")
     
 class KdTree:
 
--- src/kd_tree_tests.py
+++ src/kd_tree_tests.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     points2d = [
         [21,42],[10,100],[15,200],[20,50],[20,300]
     ]
+    y = ["","","","",""]
 
     points3d = [
         [10,0,100],[5,10,15],[7,7,7],[9,0,8],[11,10,9],[11,0,0]
@@ -17,8 +18,6 @@ if __name__ == "__main__":
 
     points = np.array(X)
     
-    kd_tree = KdTree()
-    for p in points:
-        kd_tree.add_node(p)
+    kd_tree = KdTree(points2d, y)
 
     kd_tree.display()
