db750bc7f05b1e59c24657c345f7397107fc56d0
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-05-01 19:26:53 -0300

    Change max heap to keep the k smallest elements for knn

--- src/heap.py
+++ src/heap.py
@@ -1,12 +1,21 @@
 
-class Heap:
-    def __init__(self, version="max", x=None):
-        if version not in ["min", "max"]:
-            raise ValueError("Invalid version, shoulbe be max or min")
-        self.version = version
+def left_pos(pos):
+    return 2 * pos + 1
+
+def right_pos(pos):
+    return 2 * (pos + 1)
+
+def parent_pos(pos):
+    return int((pos - 1)/2)
+
+class MaxHeap:
+    def __init__(self, x=None, k=None):
+        self.k = k
         if x is None:
             self.heap = []
         else:
+            if k is not None and len(x) > k:
+                raise ValueError("Size of x must be lesser than max size")
             self.heap = x
             self.heapify()
     
@@ -16,25 +25,16 @@ class Heap:
             s += f"{h} "
         return s
     
-    @staticmethod
-    def left_pos(pos):
-        return 2 * pos + 1
-    
-    @staticmethod
-    def right_pos(pos):
-        return 2 * (pos + 1)
-    
-    @staticmethod
-    def parent_pos(pos):
-        return int((pos - 1)/2)
-    
     def add(self, value):
         h = self.heap
-        c_pos = len(self.heap)
+        c_pos = len(h)
 
         h.append(value)
 
         self.heapify_down(c_pos)
+
+        if self.k is not None and len(h) > self.k:
+            self.remove()
     
     def remove(self):
         h = self.heap
@@ -61,7 +61,7 @@ class Heap:
         if self.is_leaf(pos):
             return
         
-        best_pos = self.compare_pos(pos, self.left_pos(pos), self.right_pos(pos))
+        best_pos = self.compare_pos(pos, left_pos(pos), right_pos(pos))
         if best_pos == pos:
             return
 
@@ -72,15 +72,14 @@ class Heap:
         if pos == 0:
             return
         
-        h = self.heap
-        p_pos = self.parent_pos(pos)
+        p_pos = parent_pos(pos)
         best_pos = self.compare_pos(pos, p_pos)
         if best_pos == pos:
             self.swap(pos, p_pos)
             self.heapify_down(p_pos)
     
     def is_leaf(self, pos):
-        return not self.is_valid_pos(self.left_pos(pos)) and not self.is_valid_pos(self.right_pos(pos))
+        return not self.is_valid_pos(left_pos(pos)) and not self.is_valid_pos(right_pos(pos))
 
     def is_valid_pos(self, pos):
         return pos <= len(self.heap) - 1
@@ -97,38 +96,40 @@ class Heap:
             indxs.append(pos2)
             values.append(h[pos2])
 
-        v = max(values) if self.version == "max" else min(values)
-        b_idx = values.index(v)
+        b_idx = values.index(max(values))
         return indxs[b_idx]
         
 
 if __name__ == "__main__":
-    h = Heap(version="max")
+    h = MaxHeap(k=3)
     h.add(22)
     h.add(8)
     h.add(13)
     h.add(3)
     h.add(56)
     h.add(90)
+    h.add(1)
 
     print(h)
-    print(h.remove())
-    print(h)
-    print(h.remove())
-    print(h)
-    print(h.remove())
-    print(h)
-    print(h.remove())
-    print(h)
 
-    x = [22,8,13,3,56,90]
-    h = Heap(x=x, version="min")
-    print(h)
-    print(h.remove())
-    print(h)
-    print(h.remove())
-    print(h)
-    print(h.remove())
-    print(h)
-    print(h.remove())
-    print(h)
+    # print(h)
+    # print(h.remove())
+    # print(h)
+    # print(h.remove())
+    # print(h)
+    # print(h.remove())
+    # print(h)
+    # print(h.remove())
+    # print(h)
+
+    # x = [22,8,13,3,56,90]
+    # h = MaxHeap(x=x, k=6)
+    # print(h)
+    # print(h.remove())
+    # print(h)
+    # print(h.remove())
+    # print(h)
+    # print(h.remove())
+    # print(h)
+    # print(h.remove())
+    # print(h)
