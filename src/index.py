d9b9843249e08789d4b63b2def6975d6fa5dba98
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-05-27 23:06:26 -0300

    Change index to generate a csv file with the results

--- src/index.py
+++ src/index.py
@@ -2,36 +2,35 @@ import csv
 import os
 
 from datetime import datetime
-from tabulate import tabulate
 from metrics_collector import collect_metrics
 from ucimlrepo.fetch import fetch_ucirepo
+from helpers.utils import get_results_directory
 
 def save_results_csv(results):
     date_str = datetime.now().strftime('%Y%m%d_%H%M%S')
 
     file_name = f"results_{date_str}.csv"
 
-    current_directory = os.getcwd()
-    directory = os.path.join(current_directory, "assets/results")
-    file_path = os.path.join(directory, file_name)
+    result_directory = get_results_directory()
+    file_path = os.path.join(result_directory, file_name)
 
-    # check if the directory exists
-    if not os.path.exists(directory):
-        # create the directory if it does not exist
-        os.makedirs(directory, exist_ok=True)
+    if not os.path.exists(result_directory):
+        os.makedirs(result_directory, exist_ok=True)
     
     keys = results[0].keys()
     with open(file_path, mode="w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=keys, delimiter=";")
         writer.writeheader()
         writer.writerows(results)
+    
+    return file_name
 
 
 if __name__ == "__main__":
     k_start, k_end, k_step = (3, 7, 2)
 
     leaf_size = 100
-    max_num_calls = 5
+    max_num_calls = 100
     methods = [
         "brute_force",
         "kd_tree",
@@ -41,16 +40,16 @@ if __name__ == "__main__":
     database_ids = [
         53,     # Iris (very low instances (very low instances - 150, low dimensions - 4) - https://archive.ics.uci.edu/dataset/53/iris
         110,    # Yeast (low instances (low instances - 1484, low dimensions - 8) - https://archive.ics.uci.edu/dataset/110/yeast
-        # 229,    # Skin Segmentation (high instances - 245057, low dimensions - 3) - https://archive.ics.uci.edu/dataset/229/skin+segmentation
-        # 31,     # Covertype (high instances - 581012, high dimensions - 54) - https://archive.ics.uci.edu/dataset/31/covertype
-        # 602,    # Dry Bean (medium instances - 13611, medium dimensions - 16) - https://archive.ics.uci.edu/dataset/602/dry+bean+dataset
-        # 372,    # HTRU2 (medium instances - 17898, low dimensions - 8) - https://archive.ics.uci.edu/dataset/372/htru2
-        # 80,     # Optical Recognition of Handwritten Digits (low instances - 5620, high dimensions - 64) - https://archive.ics.uci.edu/dataset/80/optical+recognition+of+handwritten+digits
-        # 891,    # CDC Diabetes Health Indicators (high instances - 253680, medium dimensions - 21) - https://archive.ics.uci.edu/dataset/891/cdc+diabetes+health+indicators
-        # 78,     # Page Blocks Classification (medium instances - 5473, medium dimensions - 10) - https://archive.ics.uci.edu/dataset/78/page+blocks+classification
-        # 545,    # Rice (Cammeo and Osmancik) (medium instances - 3810, medium dimensions - 7) - https://archive.ics.uci.edu/dataset/545/rice+cammeo+and+osmancik
-        # 159,    # MAGIC Gamma Telescope (medium instances - 19020, medium dimensions - 10) - https://archive.ics.uci.edu/dataset/159/magic+gamma+telescope
-        # 148,    # Statlog (Shuttle) (high instances - 58000, medium dimensions - 7) - https://archive.ics.uci.edu/dataset/148/statlog+shuttle
+        229,    # Skin Segmentation (high instances - 245057, low dimensions - 3) - https://archive.ics.uci.edu/dataset/229/skin+segmentation
+        31,     # Covertype (high instances - 581012, high dimensions - 54) - https://archive.ics.uci.edu/dataset/31/covertype
+        602,    # Dry Bean (medium instances - 13611, medium dimensions - 16) - https://archive.ics.uci.edu/dataset/602/dry+bean+dataset
+        372,    # HTRU2 (medium instances - 17898, low dimensions - 8) - https://archive.ics.uci.edu/dataset/372/htru2
+        80,     # Optical Recognition of Handwritten Digits (low instances - 5620, high dimensions - 64) - https://archive.ics.uci.edu/dataset/80/optical+recognition+of+handwritten+digits
+        891,    # CDC Diabetes Health Indicators (high instances - 253680, medium dimensions - 21) - https://archive.ics.uci.edu/dataset/891/cdc+diabetes+health+indicators
+        78,     # Page Blocks Classification (medium instances - 5473, medium dimensions - 10) - https://archive.ics.uci.edu/dataset/78/page+blocks+classification
+        545,    # Rice (Cammeo and Osmancik) (medium instances - 3810, medium dimensions - 7) - https://archive.ics.uci.edu/dataset/545/rice+cammeo+and+osmancik
+        159,    # MAGIC Gamma Telescope (medium instances - 19020, medium dimensions - 10) - https://archive.ics.uci.edu/dataset/159/magic+gamma+telescope
+        148,    # Statlog (Shuttle) (high instances - 58000, medium dimensions - 7) - https://archive.ics.uci.edu/dataset/148/statlog+shuttle
     ]
     
     dbs_data = []
@@ -77,17 +76,6 @@ if __name__ == "__main__":
                 data.update(result)
                 results.append(data)
     
-    save_results_csv(results)
-    
-    # print(tabulate(results, headers="keys", tablefmt="rounded_grid"))
-    # print()
-    # print(f"{metadata.name}, n_samples={len(X)}, n_dimensions={len(X[0])}")
-    # for result in results:
-    #     improv = 0
-    #     if result["method"] != "brute_force":
-    #         brute_predict_duration = next((item["predict_duration_seconds"] for item in results if item["method"] == "brute_force" and item["k"] == result["k"]), 0)
-    #         improv = result["predict_duration_seconds"] - brute_predict_duration if brute_predict_duration != 0 else 0
-    #     result["predict_diff_seconds"] = improv
-    # print(tabulate(results, headers="keys", tablefmt="rounded_grid"))
-    # t = f"{acc*100:.2f}%"
-    # "fit_duration_ms": (fit_duration_seconds / num_calls) * 1000,
+    file_name = save_results_csv(results)
+
+    print(f"\nGenerated csv file with results: {file_name}")
