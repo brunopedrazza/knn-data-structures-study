ac3a09513d6b0416afe984b1036764c6a5e55b34
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-06-16 19:29:21 -0300

    Add documentation to utils and implement other methods

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,39 +1,144 @@
-from time import perf_counter
-from scipy.spatial import distance
-
+import yaml
 import os
 import numpy as np
+import csv
 
+from collections import defaultdict
+from datetime import datetime
+from scipy.spatial import distance
+from tabulate import tabulate
 
-def divide_chunks(l, n):
-    for i in range(0, len(l), n):  
-        yield l[i:i + n] 
-
-
-def measure_execution_time(func):
-    def wrapper(*args, **kwargs):
-        tic = perf_counter()
-        result = func(*args, **kwargs)
-        toc = perf_counter()
-        print(f"Function {func.__qualname__} took {toc - tic:.4f} seconds to execute")
-        return result
-    return wrapper
+from helpers.dotdict import dotdict
 
 
 def euclidean_distance(p1, p2, squared=False):
+    """ Calculates the Eucledian distances between two points.
+
+    Parameters
+    ----------
+    p1 : Any
+    p2 : Any
+    squared : boolean
+        If True, it will return the squared distance.
+    """
+
     if not hasattr(p1, "shape") or len(p1.shape) == 1:
         dist = np.sum(np.square(p1 - p2))
         return dist if squared else np.sqrt(dist)
     return distance.cdist(p1, p2, 'euclidean')
 
 
-def get_results_directory():
-    current_directory = os.getcwd()
-    return os.path.join(current_directory, "assets/results")
-
-
 def find_farthest_point(X, point):
+    """ Return the farthest points in the list given a specific point.
+
+    Parameters
+    ----------
+    X : List[Any]
+        List of points to find the farthest one.
+    point : Any
+        Point to find the farthest one from.
+    """
+    
     distances = np.linalg.norm(X - point, axis=1)
     farthest_idx = np.argmax(distances)
     return X[farthest_idx]
 
+
+def get_results_directory():
+    """ Method that gets the directory where the results are. """
+
+    return "../assets/results"
+
+
+def get_results(file_name):
+    """ Fetches results data from a result file.
+
+    Parameters
+    ----------
+    file_name : str
+        Name of the file.
+    """
+    
+    result_directory = get_results_directory()
+    file_path = os.path.join(result_directory, file_name)
+
+    results = []
+    with open(file_path, mode="r", newline="") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            results.append(row)
+    
+    return results
+
+
+def print_results(file_name=None, results=None):
+    if not file_name and not results:
+        raise ValueError("File name or actual results are required")
+    
+    if not results:
+        results = get_results(file_name)
+    
+    db_groups = defaultdict(list)
+    for row in results:
+        db_groups[row["db_id"]].append(row)
+    
+    for db_id, db_results in db_groups.items():
+        db_metadata = db_results[0]
+        db_name, n_samples, n_dimensions = db_metadata["db_name"], db_metadata["n_samples"], db_metadata["n_dimensions"]
+        print()
+        print(f"id {db_id} ({db_name}), n_samples={n_samples}, n_dimensions={n_dimensions}")
+        results = []
+        for db_result in db_results:
+            accuracy = db_result["accuracy"]
+            result = {
+                "k": db_result["k"],
+                "method": db_result["method"],
+                "n_train": db_result["n_train"],
+                "n_test": db_result["n_test"],
+                "fit_duration_ms": float(db_result["fit_duration_seconds"]) * 1000,
+                "predict_duration_seconds": db_result["predict_duration_seconds"],
+                "accuracy": f"{float(accuracy)*100:.2f}%",
+                "distance_count": db_result["distance_count"],
+            }
+            improv = 0
+            if result["method"] != "brute_force":
+                brute_predict_duration = next((item["predict_duration_seconds"] for item in results if item["method"] == "brute_force" and item["k"] == result["k"]), 0)
+                improv = float(result["predict_duration_seconds"]) - float(brute_predict_duration) if float(brute_predict_duration) != 0 else 0
+            result["predict_diff_seconds"] = improv
+            results.append(result)
+        print(tabulate(results, headers="keys", tablefmt="rounded_grid"))
+        
+
+def save_results_csv(results):
+    """ Save results in a csv file.
+
+    Parameters
+    ----------
+    results : Any
+        Results data.
+    """
+
+    date_str = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+    file_name = f"results_{date_str}.csv"
+
+    result_directory = get_results_directory()
+    file_path = os.path.join(result_directory, file_name)
+
+    if not os.path.exists(result_directory):
+        os.makedirs(result_directory, exist_ok=True)
+    
+    keys = results[0].keys()
+    with open(file_path, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=keys, delimiter=";")
+        writer.writeheader()
+        writer.writerows(results)
+    
+    return file_name
+
+
+def load_parameters():
+    parameters_path = "../parameters.yaml"
+    with open(parameters_path, mode="r") as file:
+        params = yaml.load(file, Loader=yaml.SafeLoader)
+    return dotdict(params)
