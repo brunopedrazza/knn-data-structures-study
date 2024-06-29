a52522175d85e2966e209d9368b081be03d549e6
Bruno Pedrazza <pedrazzabruno@gmail.com>
2024-06-29 13:08:43 -0300

    Change ball tree construction

--- src/trees/nodes/balltreenode.py
+++ src/trees/nodes/balltreenode.py
@@ -23,7 +23,6 @@ class BallTreeNode(Node):
     The norm vector is used to avoid recalculating it for every target point.
 
     """
-            
     def __init__(self, X, X_idx, leaf_size):
         """ Init method to construct the tree structure.
 
@@ -41,35 +40,24 @@ class BallTreeNode(Node):
         self.X_idx = None
         self.is_leaf = False
         self.left = self.right = None
-        self.median = None
-        self.vector_norm = None
+        self.centroid = None
+        self.radius = None
         
+        self.centroid = np.mean(X, axis=0)
+        pleft = find_farthest_point(X, self.centroid)
+        self.radius = np.linalg.norm(self.centroid - pleft, axis=0)
+
         n = X.shape[0]
+
         if n <= leaf_size:
             self.create_leaf(X, X_idx)
             return
-        
 
-        rand_idx = np.random.randint(n)
-        rand_point = X[rand_idx]
-        
-        # find the most distant point from the randomly picked point
-        x0 = find_farthest_point(X, rand_point)
-        
-        # find the point most distant from the farthest point
-        x1 = find_farthest_point(X, x0)
+        pright = find_farthest_point(X, pleft)
+        distances_to_pleft = np.linalg.norm(X - pleft, axis=1)
+        distances_to_pright = np.linalg.norm(X - pright, axis=1)
 
-        # calculate the line vector between them
-        self.line_vector = x1 - x0
-        
-        # project all points onto this line
-        self.vector_norm = np.linalg.norm(self.line_vector)
-        projections = np.dot(X, self.line_vector) / self.vector_norm
-        
-        # split based on the median of the projections
-        self.median = np.median(projections)
-        left_idx = projections <= self.median
-        right_idx = projections > self.median
-        
-        self.left = BallTreeNode(X[left_idx], X_idx[left_idx], leaf_size)
-        self.right = BallTreeNode(X[right_idx], X_idx[right_idx], leaf_size)
+        belongs_left = distances_to_pleft < distances_to_pright
+
+        self.left = BallTreeNode(X[belongs_left], X_idx[belongs_left], leaf_size)
+        self.right = BallTreeNode(X[~belongs_left], X_idx[~belongs_left], leaf_size)
